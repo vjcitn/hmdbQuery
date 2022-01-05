6b8075397a20d0ecc5ac3ea199ab937789bbb100
vjcitn <stvjc@channing.harvard.edu>
2022-01-05 13:02:48 -0500

    repair test on number of tissues (which changed at host) for a chemical

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -6,7 +6,7 @@ test_that("query resolves", {
  lk1 = HmdbEntry()
  expect_true(class(lk1)=="HmdbEntry")
  expect_true(length(diseases(lk1))>2)
- expect_true(length(tissues(lk1))>1)
+ expect_true(length(tissues(lk1))>=1)
  expect_true(length(biospecimens(lk1))>2)
 })
 
